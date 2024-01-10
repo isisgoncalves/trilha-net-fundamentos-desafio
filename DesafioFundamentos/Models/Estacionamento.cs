b973159c6d17b308f8f01fafb1c03dbbb336ba06
isisgoncalves <isisvow@gmail.com>
2024-01-10 09:47:32 -0300

    Update Estacionamento.cs
    Add veiculo

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -14,9 +14,11 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placa = Console.ReadLine();
+
+            veiculos.Add(placa.ToUpper());
+            Console.WriteLine($"Veículo com placa {placa} foi estacionado com sucesso.");
         }
 
         public void RemoverVeiculo()
